854abd6691601aa166d28a23f4f2eab83f643a07
LucasMMF <mateusmidao@gmail.com>
2023-04-30 09:10:21 -0300

    feat(swagger): adding contact information

--- src/main/java/br/com/myrmidao/api/config/SwaggerConfig.java
+++ src/main/java/br/com/myrmidao/api/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
@@ -16,6 +17,12 @@ public class SwaggerConfig {
 			new Info()
 				.title("API Usuários - COTI Informática")
 				.description("API Spring Boot para controle de usuários.")
+				.contact(
+					new Contact()
+						.name("Lucas Mateus Midão Fernandes")
+						.email("mateusmidao@gmail.com")
+						.url("https://github.com/LucasMMF")
+				)
 		);
 	}
 	
